1c76ce73590fe89b92ee0ffb9afe1ecc756b259c
rtkr <r.tuckner@gmail.com>
2017-12-27 16:43:55 -0600

    added post scaffold

--- db/migrate/20171227224020_create_posts.rb
+++ db/migrate/20171227224020_create_posts.rb
@@ -1,8 +1,10 @@
-class AddPosts < ActiveRecord::Migration[5.1]
+class CreatePosts < ActiveRecord::Migration[5.1]
   def change
     create_table :posts do |t|
       t.string :title
       t.text :body
+
+      t.timestamps
     end
   end
 end
